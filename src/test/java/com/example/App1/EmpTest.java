249d7a6373f44924dd1d515cbb2286e435480c57
D.K <on.registry@gmail.com>
2016-12-04 11:05:04 +0900

    Emp

---
+++ src/test/java/com/example/App1/EmpTest.java
@@ -0,0 +1,11 @@
+package com.example.App1;
+
+import junit.framework.TestCase;
+
+public class EmpTest extends TestCase {
+
+	   public void testEmp()
+	    {
+	        assertTrue( true );
+	    }
+}
