f371c202e1d143dee4933d5e430c2b080cf771de
D.K <on.registry@gmail.com>
2016-12-04 12:04:23 +0900

    test

--- src/main/java/com/example/App1/App.java
+++ src/main/java/com/example/App1/App.java
@@ -4,10 +4,10 @@ package com.example.App1;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!2" );
     }
 }
